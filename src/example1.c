9c3ee7b048477e329c8dee6cbb3ccf94e3fcb44b
Kurt Stephens <git@kurtstephens.com>
2021-08-27 21:27:20 -0500

    Code clean up.

--- src/example1.c
+++ src/example1.c
@@ -1,6 +1,5 @@
 #include "example1.h"
 
 double cubic_poly(double x, double c0, double c1, double c2, double c3) {
-  return c0 + (x * c1) + (x * x * c2) + (x * x * x * c3);
+  return c0 + c1*x + c2*x*x + c3*x*x*x;
 }
-
