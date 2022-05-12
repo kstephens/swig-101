b1060bcc95829b7c8a4ec83bdcb45d4134d1a281
Kurt Stephens <kstephens@DRWHoldings.com>
2022-05-11 19:38:40 -0500

    Added #define POLYNOMIAL_VERSION.

--- src/polynomial.h
+++ src/polynomial.h
@@ -1,8 +1,9 @@
 #include <vector>
-
+#define POLYNOMIAL_VERSION "2.3.5"
 class Polynomial {
  public:
   std::vector<double> coeffs;
+  /* Returns: coeffs[0] + coeefs[1] * pow(x, 1) + coeefs[2] * pow(x, 2) ... */
   double evaluate(double x);
 };
 
